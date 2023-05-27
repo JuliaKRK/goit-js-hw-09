3999e492b7979bf068df9cdb0d3c55d0151e8dd2
unknown <ulyamba@gmail.com>
2023-05-27 12:10:53 +0200

    DZ JS 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId;
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startButton.disabled = true;
+}
+
+function stopColorChange() {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+}
+
+function changeBackgroundColor() {
+  const body = document.body;
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+const inputDelayEl = document.querySelector('input[name=delay]');
+const inputDelayStepEl = document.querySelector('input[name=step]');
+const inputAmountPromisesEl = document.querySelector('input[name=amount]');
+
+formEl.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const delayValue = Number(inputDelayEl.value);
+  const stepValue = Number(inputDelayStepEl.value);
+  const amountValue = Number(inputAmountPromisesEl.value);
+  let delay = delayValue;
+  let position = 0;
+  delay -= stepValue;
+  formEl.reset();
+  for (let i = 0; i < amountValue; i += 1) {
+    delay += stepValue;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
